b57c4c839ad296b0d089542dbad876a80514938b
putyWang <13540025344@163.com>
2023-06-25 15:57:56 +0800

    增加变量相关笔记

--- foundation/variable/src/main.rs
+++ foundation/variable/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    let x = 5;
+    // 若需要对 x 进行二次赋值 需要使用 mut 进行修饰 
+    // let x = 5;
+    // const 关键字与申明常量 也是不可变的 const x = 5;
+    let mut x = 5;
     println!("The value of x is: {x}");
+    // x 未用 mut修饰时，编译时报错 cannot assign twice to immutable variable `x`
     x = 6;
     println!("The value of x is: {x}");
 }
